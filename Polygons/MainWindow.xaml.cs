df562a100218828014b6da9067ce8ec0ffae2790
Parviz Abdulloev <parviz23.10@inbox.ru>
2019-05-18 21:31:19 +0300

    add exception handling

--- Polygons/MainWindow.xaml.cs
+++ Polygons/MainWindow.xaml.cs
@@ -58,74 +58,80 @@ namespace Polygons
                                 + Environment.NewLine
                                 + "x1 y1 x2 y2 x3 y3", "Формат");
             }
-            
 
-            using (var streamReader = new StreamReader(openFileDialog.FileName)) // открываем файл на чтения
+            try
             {
-                while (!streamReader.EndOfStream)
+                using (var streamReader = new StreamReader(openFileDialog.FileName)) // открываем файл на чтения
                 {
-                    string str = streamReader.ReadLine(); // читаем 1 строку
-                    perimeter = 0; // обнуляем периметр для нового многоугольника
-
-                    numbers = str.Split(' ');  // добавляем каждую коориднату в массив с помощью Split
-
-                    if ((numbers.Length >= 3) && (numbers.Length % 2 == 0)) // если количество вершин >=3 и количество вершин четное количество
+                    while (!streamReader.EndOfStream)
                     {
+                        string str = streamReader.ReadLine(); // читаем 1 строку
+                        perimeter = 0; // обнуляем периметр для нового многоугольника
 
-                        for (byte i = 0; i <= (numbers.Length - 3); i += 2) // находим периметр
+                        numbers = str.Split(' ');  // добавляем каждую коориднату в массив с помощью Split
+
+                        if ((numbers.Length >= 3) && (numbers.Length % 2 == 0)) // если количество вершин >=3 и количество вершин четное количество
                         {
-                            int numberX1 = int.Parse(numbers[i]);
-                            int numberY1 = int.Parse(numbers[i + 1]);
 
-                            int numberX2 = int.Parse(numbers[i + 2]);
-                            int numberY2 = int.Parse(numbers[i + 3]);
+                            for (byte i = 0; i <= (numbers.Length - 3); i += 2) // находим периметр
+                            {
+                                int numberX1 = int.Parse(numbers[i]);
+                                int numberY1 = int.Parse(numbers[i + 1]);
 
-                            double distance = Math.Sqrt(((numberX2 - numberX1) * (numberX2 - numberX1)) + ((numberY2 - numberY1) * (numberY2 - numberY1))); // длина 
+                                int numberX2 = int.Parse(numbers[i + 2]);
+                                int numberY2 = int.Parse(numbers[i + 3]);
 
-                            perimeter += distance;
+                                double distance = Math.Sqrt(((numberX2 - numberX1) * (numberX2 - numberX1)) + ((numberY2 - numberY1) * (numberY2 - numberY1))); // длина 
+
+                                perimeter += distance;
+                            }
+
+                            // переменные для нахожения площади многоугольника
+                            int x1 = int.Parse(numbers[0]);
+                            int y1 = int.Parse(numbers[1]);
+                            double sum = 0;
+                            int x2 = 0;
+                            int y2 = 0;
+
+                            for (byte i = 0; i < (numbers.Length - 4); i++) // находим площадь
+                            {
+                                x2 = int.Parse(numbers[i + 2]);
+                                y2 = int.Parse(numbers[i + 3]);
+                                sum += (x1 + x2) * (y2 - y1);
+                                x1 = x2;
+                                y1 = y2;
+                            }
+
+                            sum += (int.Parse(numbers[0]) + x2) * (int.Parse(numbers[1]) - y2);
+
+
+
+                            polygon = new Polygon()
+                            {
+                                CountOfPoints = Convert.ToString(numbers.Length / 2),
+                                Perimeter = Convert.ToString(perimeter),
+                                Square = Convert.ToString(Math.Abs(sum / 2))
+                            };
+
+                            list.Add(polygon);
                         }
 
-                        // переменные для нахожения площади многоугольника
-                        int x1 = int.Parse(numbers[0]); 
-                        int y1 = int.Parse(numbers[1]);
-                        double sum = 0;
-                        int x2 = 0;
-                        int y2 = 0;
-
-                        for (byte i = 0; i < (numbers.Length-4); i++) // находим площадь
+                        else
                         {
-                            x2 = int.Parse(numbers[i + 2]);
-                            y2 = int.Parse(numbers[i + 3]);
-                            sum += (x1 + x2) * (y2 - y1);
-                            x1 = x2;
-                            y1 = y2;
+                            polygon = new Polygon()
+                            {
+                                CountOfPoints = "Ошибка",
+                                Perimeter = "Ошибка",
+                                Square = "Ошибка"
+                            };
+                            list.Add(polygon);
                         }
-
-                        sum += (int.Parse(numbers[0]) + x2) * (int.Parse(numbers[1]) - y2);
-
-
-
-                        polygon = new Polygon()
-                        {
-                            CountOfPoints = Convert.ToString(numbers.Length / 2),
-                            Perimeter = Convert.ToString(perimeter),
-                            Square = Convert.ToString(Math.Abs(sum / 2))
-                        };
-
-                        list.Add(polygon);
-                    }                  
-
-                    else  
-                    {
-                        polygon = new Polygon()
-                        {
-                            CountOfPoints = "Ошибка",
-                            Perimeter = "Ошибка",
-                            Square = "Ошибка"
-                        };
-                        list.Add(polygon);
                     }
                 }
+            }
+            catch (System.ArgumentException)
+            {
+                MessageBox.Show("Вы не выбрали файл!");
             }
             ListView.ItemsSource = list;
         }
